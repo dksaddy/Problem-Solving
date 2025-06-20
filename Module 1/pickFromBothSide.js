3f5f87a829ecc419b80b7221004f81f6db56a44b
dksaddy <saddypro1@gmail.com>
2025-06-20 11:54:24 +0600

    Revised: Pick From Both Sides of an Array

--- Module 1/pickFromBothSide.js
+++ Module 1/pickFromBothSide.js
@@ -1,15 +1,30 @@
+/*
+Problem Description:
+
+1. Given an integer array A of size N.
+2. You have to pick exactly B elements from either left or right end of the array 
+    A to get the maximum sum.
+3. Find and return this maximum possible sum.
+4. NOTE: Suppose B = 4 and array A contains 10 elements then
+5. You can pick the first four elements 
+   or can pick the last four elements 
+   or can pick 1 from the front and 3 from the back etc. 
+   ###you need to return the maximum possible sum of elements you can pick.###
+*/
+
 const array = [5, -2, 3, 1, 2];
 let n = 3;
-let maxSum = 0;
+//Output: 8
 
-// Start by taking first n elements
+let maxSum = 0;
 let currentSum = 0;
+
 for (let i = 0; i < n; i++) {
   currentSum += array[i];
 }
+
 maxSum = currentSum;
 
-// Try all combinations: take i from front and n-i from back
 for (let i = 1; i <= n; i++) {
   currentSum = currentSum - array[n - i] + array[array.length - i];
   maxSum = Math.max(maxSum, currentSum);
@@ -17,14 +32,3 @@ for (let i = 1; i <= n; i++) {
 
 console.log("Maximum sum by picking from both sides:", maxSum);
 
-
-/*
-currentSum = 6
-maxSum = 6
-Line 14: currentSum = 6 - 3 + 2 = 5
-currentSum = 5
-maxSum = 6
-Line 14: currentSum = 5 - (-2) + 1 = 8
-maxSum = 8
-// The maximum sum by picking 3 elements from both sides is 8
-*/ 
