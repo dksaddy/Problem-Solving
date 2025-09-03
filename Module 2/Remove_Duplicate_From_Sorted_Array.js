eb6c211af881473404128998ff41de1d502eba96
dksaddy <saddypro1@gmail.com>
2025-09-03 20:46:03 +0600

    Remove duplicates from sorted array: Module 2

---
+++ Module 2/Remove_Duplicate_From_Sorted_Array.js
@@ -0,0 +1,14 @@
+const num = [1,1,2];
+
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
+
+console.log(removeDuplicates(num));
